461b1095cb4324c517cc3d8651953ae291616fd6
Nadher Arroum <nadher.arroum@gmail.com>
2021-12-24 15:06:18 +0100

    Initial project

---
+++ src/main/java/com/nadherarroum/proj_ds/ProjDsApplication.java
@@ -0,0 +1,13 @@
+package com.nadherarroum.proj_ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjDsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nadherarroum/proj_ds/ProjDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadherarroum.proj_ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjDsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
